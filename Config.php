70e74cccef5d170279623c4d5a9953b3a5ace6b7
1102137239 <1102137239@gm.kuas.edu.tw>
2016-12-23 10:21:36 +0900

    KLife_V1.0.0

--- Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db_host = 'db.mis.kuas.edu.tw';
-$db_user = 's1102137204';
-$db_pass = 'D222759151';
-$db_name = 's1102137204';
-$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
-
-
